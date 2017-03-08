02db779102c3b832526a829e325d762d0ac8516c
iloster <243849707@qq.com>
2017-03-09 00:41:27 +0800

    =爬取v2ex

--- app/Http/Controllers/V2exController.php
+++ app/Http/Controllers/V2exController.php
@@ -23,12 +23,30 @@ class V2exController extends Controller
         Log::info($body);
         $pattern = '/<div class=\"cell from[\s\S]*?<span class=\"item_title\">([\s\S]*?)<\/span>[\s\S]*?<span class=\"small fade\">([\s\S]*?)<\/span>/';
         preg_match_all($pattern,$body,$matches);
-//        echo count($matches[0]);
-//        dump($matches[0]);
-        for($i=1;$i<count($matches);$i++){
-            dump($matches[$i]);
-//            dump($matches[2]);
+
+//        $data  = array();
+        Log::info("count:".count($matches));
+        if(count($matches)>1){
+            for($i = 0;$i<count($matches[1]);$i++){
+                $item['title'] = $matches[1][$i];
+                $temp = explode("<strong>", $matches[2][$i]);
+                if(count($temp)===3){
+                    $item['last_reply'] =preg_replace("/<(.*?)>/","",$temp[2]);
+                }else{
+                    $item['last_reply'] ="" ;
+                }
+                $temp1 = explode("</strong>", $temp[1]);
+                $item['author'] =preg_replace("/<(.*?)>/","",$temp1[0]);
+                $item['public_time'] = str_replace('&nbsp;','',$temp1[1]);
+                dump($item);
+            }
         }
+//        for($i=1;$i<count($matches);$i++){
+//            dump($matches[$i]);
+//            for($j = 0;$j<=9;$j++) {
+//                dump(explode("<strong>", $matches[2][$j]));
+//            }
+//        }
 
     }
 }
