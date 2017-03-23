b6c34e8ee11df41b811d967a9c38409c6b81f8c6
iloster <243849707@qq.com>
2017-03-23 22:21:00 +0800

    =

--- app/Http/Controllers/HuXiuController.php
+++ app/Http/Controllers/HuXiuController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\HuxiuModel;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Log;
 use GuzzleHttp\Client;
-class HuXiuController extends Controller
+class HuxiuController extends Controller
 {
     //
     const URL = "http://m.api.huxiu.com/portal/%s/%s";
@@ -17,9 +18,14 @@ class HuXiuController extends Controller
         Log::info('url:'.$url);
         $client = new Client();
         $ret = $client->get($url)->getBody();
-        Log::info('ret:'.$ret);
-        $retArr = json_decode($ret);
-
+//        Log::info('ret:'.$ret);
+        $retArr = json_decode($ret,true);
+        if(count($retArr['content'])>0){
+            $model = new HuxiuModel();
+            $model->insertNews($retArr['content']);
+        }
         return $ret;
     }
+
+
 }
---
+++ app/HuxiuModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HuxiuModel extends Model
+{
+    //
+    protected $table="huxiu_news";
+    protected $fillable = ['aid','title','summary','author_id','author','img','dateline'];
+    public $timestamps = false;
+
+    public function insertNews($items){
+        foreach($items as $item){
+            $flag = $this::where('aid',$item['aid'])->get();
+            if(count($flag)==0){
+                $temp = array();
+                $temp['aid'] = $item['aid'];
+                $temp['title'] = $item['title'];
+                $temp['summary'] = $item['summary'];
+                $temp['author_id'] = $item['author_id'];
+                $temp['img'] = $item['img'];
+                $temp['dateline'] = $item['dateline'];
+                $this::insert($temp);
+            }
+        }
+    }
+}
