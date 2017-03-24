b747b3fb3c7eced28a473a24672beee3f4ce89fd
iloster <243849707@qq.com>
2017-03-24 22:22:01 +0800

    =

--- app/Http/Controllers/XituController.php
+++ app/Http/Controllers/XituController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\XituModel;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,23 @@ class XituController extends Controller
 
         $client = new Client();
         $ret = $client->get(self::URL)->getBody();
+        $retArr = json_decode($ret,true);
+        if(count($retArr['data']['entry'])>0){
+            $items = array();
+            foreach ($retArr['data']['entry'] as $item) {
+                $temp = array();
+                $temp['objectId'] = $item['objectId'];
+                $temp['title'] = $item['title'];
+                $temp['content'] = $item['content'];
+                $temp['url'] = $item['url'];
+                $temp['username'] = $item['user']['username'];
+                $temp['avatar'] = $item['user']['avatar_large'];
+                $temp['updatedAt'] = $item['updatedAt'];
+                $items[] = $temp;
+            }
+            $model = new XituModel();
+            $model->insertHotNews($items);
+        }
         return $ret;
     }
 }
--- app/XituModel.php
+++ app/XituModel.php
@@ -10,4 +10,21 @@ class XituModel extends Model
     protected $table="xitu_news";
     protected $fillable = ['objectId','title','content','url','username','avatar','updatedAt'];
     public $timestamps = false;
+
+    public function insertHotNews($items){
+        foreach($items as $item){
+            $flag = $this::where('objectId',$item['objectId'])->get();
+            if(count($flag)==0){
+                $temp = array();
+                $temp['objectId'] = $item['objectId'];
+                $temp['title'] = $item['title'];
+                $temp['content'] = $item['content'];
+                $temp['url'] = $item['url'];
+                $temp['username'] = $item['username'];
+                $temp['avatar'] = $item['avatar'];
+                $temp['updatedAt'] = strtotime($item['updatedAt']);
+                $this::insert($temp);
+            }
+        }
+    }
 }
