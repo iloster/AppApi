b2e771eadaa9dbe9d69ea52639a12b5f26212c11
loster <243849707@qq.com>
2017-03-27 15:30:05 +0800

    =

---
+++ app/Http/Controllers/FanfouController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class FanfouController extends Controller
+{
+    //http://blog.fanfou.com/digest/json/2015-11-13.daily.json
+}
---
+++ app/Http/Controllers/QiubaiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class QiubaiController extends Controller
+{
+    //http://m2.qiushibaike.com/article/list/latest
+    //http://m2.qiushibaike.com/article/list/suggest
+}
